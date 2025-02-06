7db75532d2c95e2e0ef315c7d6ef8059c8ec7502
preistone <aion97@naver.com>
2025-02-06 18:51:07 +0900

    create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100px;
+`;
+
+const Header = () => {
+  return <Container></Container>;
+};
+
+export default Header;
