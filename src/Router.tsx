8fffbd7d8d6e6e1ae1ef0b9eddd09b34d9a8fdad
preistone <aion97@naver.com>
2025-02-07 15:31:37 +0900

    Header import

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Edit from "./pages/Edit";
 
 const Router = () => {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/edit" element={<Edit></Edit>}></Route>
       </Routes>
     </HashRouter>
   );
---
+++ src/pages/Edit.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Header from "../components/Header";
+
+const Container = styled.div``;
+
+const Edit = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Edit;
