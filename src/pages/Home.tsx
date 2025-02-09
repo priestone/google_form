7e4f1d5702e3365b28549a46e10b149f6ffc5465
preistone <aion97@naver.com>
2025-02-09 23:36:53 +0900

    link edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   width: 100%;
@@ -9,6 +10,7 @@ const Home = () => {
   return (
     <Container>
       <Header />
+      <Link to={"/edit"}>에딧가기</Link>
       홈입니다.
     </Container>
   );
